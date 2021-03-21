c5ec94cec4f150ee75df53316478782361f34a69
fernandacabre <80422657+fernandacabre@users.noreply.github.com>
2021-03-21 20:39:15 -0300

    Completé "ejercicios_practica.py" del modulo 2

--- ejercicios_practica.py
+++ ejercicios_practica.py
@@ -24,13 +24,16 @@ def ej1():
     # numero_1 y numero_2
     # Almacenar el valor de la suma en una variable
     # ej:
-    # operacion = .....
+    suma = numero_1 + numero_2
 
     # Imprimir en pantalla el resultado de la suma
-    # print(....)
+    print("El resultado de la suma es: ", suma)
 
     # Repita el procedimiento para realizar la resta
-
+    resta = numero_1 - numero_2
+    
+    # Imprimir en pantalla el resultado de la resta
+    print("El resultado de la resta es: ", resta)
 
 def ej2():
     # Ejercicios de práctica numérica
@@ -38,28 +41,38 @@ def ej2():
     # Ahora los valores a operar deben ser ingresados por
     # consola con la función "input" como se ve a continuación
     print('Ingrese el primer número decimal a operar:')
-    numero_1 = int(input())
+    numero_1 = float(input())
 
     print('Ingrese el segundo número decimal a operar:')
-    numero_2 = int(input())
+    numero_2 = float(input())
 
     # Alumno: Imprima en pantalla los dos números decimales solicitados
-    # print(....)
+    print("El primer numero ingresado es: ", numero_1)
+    print("El segundo numero ingresado es: ", numero_2)
 
     # Alumno: Calcule la suma, resta, división y multiplicación de los números ingresados
     # numero_1, numero_2
+    # Suma
+    suma = numero_1 + numero_2
+    
+    # Resta
+    resta = numero_1 - numero_2
+    
+    # Multiplicación
+    multiplicacion = numero_1 * numero_2
+    
+    # División
+    division = numero_1 / numero_2
+
     # Imprima en pantalla todos los resultados con el siguiente formato de ejemplo:
     # El resultado de sumar 4 y 2 es 6
+
+    print("El resultado de sumar", numero_1, "y", numero_2, "es", suma)
+    print("El resultado de restar", numero_1, "y", numero_2, "es", resta)
+    print("El resultado de multiplicar", numero_1, "y", numero_2, "es", multiplicacion)
+    print("El resultado de dividir", numero_1, "y", numero_2, "es", division)
+
     # NOTA: No coloque usted los nùmeros y resultados, use las variables
-
-    # Suma
-
-    # Resta
-
-    # División
-
-    # Multiplicación
-
 
 def ej3():
     # Ejemplos variables de texto
@@ -73,12 +86,15 @@ def ej3():
     apellido = str(input())
 
     # Imprima su nombre completo
+    print('Mi nombre completo es:', nombre)
 
     # Almacenar su nombre completo en una variable
     # nombre_completo = .....
 
     # Imprimir la cantidad de letras que posee su nombre completo
-
+    nombre_completo = "Maria Fernanda"
+    nombre_completo_len = len(nombre_completo)
+    print ("Mi nombre completo tiene", nombre_completo_len, "letras")
 
 def ej4():
     # Ejemplos variables de texto
@@ -96,9 +112,13 @@ def ej4():
 
     # De cada palabra debe tomar la primera letra y armar el acrónimo
     # Ejemplo: Alumbrado, barrido y limpieza --> ABL
+    primera_letra_1 = palabra_1[0]
+    primera_letra_2 = palabra_2[0]
+    primera_letra_3 = palabra_3[0]
+ 
     # Imprimir el resultado en pantalla
-
-
+    print(primera_letra_1,primera_letra_2,primera_letra_3)
+ 
 def ej5():
     # Ejemplos variables de texto
 
@@ -110,15 +130,27 @@ def ej5():
     palabra_2 = str(input())
 
     # De la primera palabra tome las primeras tres letras, utilice el operador :
+    sub_text1 = palabra_1[:3]
+    
     # De la segunda palabra tome las últimas tres letras, utilice el operador :
+    palabra_2_len = len(palabra_2)
+    sub_text2 = palabra_2[4:palabra_2_len]
+    
     # Formar una nueva palabra con los recortes solicitados
-    # Imprima en pantalla los resultados
+    nueva_palabra = sub_text1 + sub_text2
 
+    # Imprima en pantalla los resultados
+    
+    print("Las primeras tres letras de palabra_1 son", sub_text1)
+    print("Las ultimas tres letras de palabra_2 son", sub_text2)
+    print("La nueva palabra es: ", nueva_palabra)
 
 if __name__ == '__main__':
+
+
     print("Bienvenidos a otra clase de Inove con Python")
     ej1()
-    # ej2()
-    # ej3()
-    # ej4()
-    # ej5()
+    ej2()
+    ej3()
+    ej4()
+    ej5()
